9b4ce7cf4894f7e8275a66a4ad63467e4bfde669
Safiyya H <safiyya.hasanova@gmail.com>
2021-09-12 13:49:37 -0500

    get single thoughts

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -12,3 +12,22 @@ const thoughtController = {
         res.status(400).json(err);
       });
   },
+
+   //get single thought by id
+   getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .populate({ path: "reactions", select: "-__v" })
+      .select("-__v")
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res
+            .status(404)
+            .json({ message: "Thought with that ID does not exist" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  },
