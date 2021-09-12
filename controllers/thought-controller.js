f215964d1c1c32d2ae15d09f241e316bf90329a3
Safiyya H <safiyya.hasanova@gmail.com>
2021-09-12 13:50:06 -0500

    crud

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -31,3 +31,64 @@ const thoughtController = {
         res.status(400).json(err);
       });
   },
+  //create new thought
+  createThought({ body }, res) {
+    Thought.create(body)
+      .then((dbThoughtData) => {
+        User.findOneAndUpdate(
+          { _id: body.userId },
+          { $push: { thoughts: dbThoughtData._id } },
+          { new: true }
+        )
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: "No user found with this id" });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => {
+            res.json(err);
+          });
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  },
+
+  //update thought by id
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(400).json({ message: "Thought ID does not exist" });
+          return;
+        }
+        res.json(dbThoughtData);
+      }).catch((err) => {
+        res.status(400).json(err);
+      });
+  },
+  // delete thought by id
+  deleteThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(400).json({ message: "Thought ID does not exist" });
+          return;
+        }
+        User.findOneAndUpdate(
+          { username: dbThoughtData.username },
+          { $pull: { thoughts: params.id } }
+        )
+          .then(() => {
+            res.json({ message: "Thought has been deleted" });
+          })
+          .catch((err) => {
+            res.status(400).json(err);
+          });
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  },
