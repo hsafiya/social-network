2e461f1e86a50df592535852cb75ed130c5a3b07
Safiyya H <safiyya.hasanova@gmail.com>
2021-09-12 13:44:36 -0500

    thought schema

--- Models/Thought.js
+++ Models/Thought.js
@@ -31,3 +31,41 @@ const ReactionSchema = new Schema(
       }
     }
   );
+  const ThoughtSchema = new Schema (
+    {
+        thoughtText:{
+            type:String,
+            required: true,
+            minLength:1,
+            maxLength:350
+        },
+        createdAt:{
+            type:Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username:{
+            type: String,
+            required:true,
+            ref: "User"
+        },
+        reaction: [ReactionSchema]
+    },
+    {
+        toJSON:{
+            virtuals:true,
+            getters:true
+        },
+        id:false
+    }
+ );
+
+ 
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+// create the Thought model using the ThoughtSchema
+const Thought = model('Thought', ThoughtSchema);
+// export the Thought model
+module.exports = Thought;
